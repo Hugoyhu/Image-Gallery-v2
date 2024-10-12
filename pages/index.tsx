4e30f09830a3c25683159e813a3638f3964c490f
Hugo Hu <hugo.y.hu@hotmail.com>
2024-10-11 23:48:24 -0400

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,174 +1,170 @@
-import Image from "next/image";
-import Link from "next/link";
-import { Inter } from "next/font/google";
-import client from "@/lib/mongodb";
-import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
+import clientPromise from "../lib/mongodb";
+import { GetServerSideProps } from 'next';
+import styles from './styles/styles.module.css';
 
-type ConnectionStatus = {
-  isConnected: boolean;
+interface Image {
+    _id: string;
+    link: string;
+    Manufacturer: string;
+    Model: string;
+    Time: Date;
+    Width: number;
+    Height: number;
+    Exposure: string;
+    FNumber: string;
+    Lens: string;
+    Focal: string;
+    ISO: number;
+    Label: string;
+    location: string;
+    featured: boolean;
+}
+interface ImageProps {
+   images: Image[];
+}
+
+const Images: React.FC<ImageProps> = ({ images }) => {
+
+    let photoList : { [key: string]: any }[] = [];
+
+    for (let i = 0; i < images.length; i++) {
+        let image = images[i];
+        
+        photoList.push(
+            {
+                "link": image.link,
+                "Lens": image.Lens,
+                "Manufacturer": image.Manufacturer,
+                "Model": image.Model,
+                "Focal": image.Focal,
+                "Aperture": image.FNumber,
+                "Exposure": image.Exposure,
+                "ISO": image.ISO,
+                "Location": image.location,
+                "Date": formatTimecode(String(image.Time)),
+                "Optimized": image.link.slice(0, 49) + "w_1200,f_auto/" + image.link.slice(49)
+            }
+        )
+    }
+
+    let column0 = [];
+    let column1 = [];
+    let column2 = [];
+
+    for (let i = 0; i < photoList.length; i++) {
+        let photo = photoList[i];
+
+        if (i % 2 == 0) {
+            column0.push(photo);
+        } else if (i % 2 == 1) {
+            column1.push(photo);
+        } 
+    }
+
+
+    return (
+        <div> 
+            <div className="text-center p-8">
+                <h1 className="text-3xl font-bold">HUGO HU</h1>
+                
+                <h3>
+                    <a href="https://www.hugohu.me" className="text-blue-500 underline">
+                        https://www.hugohu.me
+                    </a>
+                </h3>
+                <h3>
+                    <a href="mailto:photography@hugohu.me" className="text-blue-500 underline">
+                        photography@hugohu.me
+                    </a>
+                </h3>
+                    
+
+                {/* <h3 className="mt-6">Sort By:</h3>
+                <div className="mt-2">
+                    <a href="/" className="text-blue-500 underline">Time (Newest First)</a>&nbsp;&nbsp;
+                    <a href="/random" className="text-blue-500 underline">Random Order</a>
+                </div>
+
+                <div className="mt-4">
+                    <a href="/category/landscape" className="text-blue-500 underline">Landscape</a>&nbsp;&nbsp;
+                    <a href="/category/nature" className="text-blue-500 underline">Nature</a>&nbsp;&nbsp;
+                    <a href="/category/birds" className="text-blue-500 underline">Birds</a>&nbsp;&nbsp;
+                    <a href="/category/animals" className="text-blue-500 underline">Animals</a>&nbsp;&nbsp;
+                    <a href="/category/metropolitan" className="text-blue-500 underline">Metropolitan</a>&nbsp;&nbsp;
+                    <a href="/category/flowers" className="text-blue-500 underline">Flowers</a>&nbsp;&nbsp;
+                    <a href="/category/craft" className="text-blue-500 underline">Craft</a>&nbsp;&nbsp;
+                    <a href="/category/astrophotography" className="text-blue-500 underline">Astro</a>
+                </div> */}
+            </div>
+
+            <div className="flex flex-row justify-between">
+            <div className="flex flex-col w-1/2 px-1">
+                {column0.map((photo, index) => (
+                <div key={index} className="flex flex-col items-center mb-4">
+                <a href={photo.link} target="_blank" rel="noopener noreferrer">
+                    <img src={photo.link} style={{ width: '100%' }}/>
+                </a>
+                <figcaption className="bg-black text-white italic p-2 text-center w-full">
+                    {photo.Model}, {photo.Lens}
+                    <br />
+                    {photo.Focal} <span>&#183;</span> {photo.Aperture} <span>&#183;</span> {photo.Exposure}s <span>&#183;</span> ISO {photo.ISO}
+                    <br />
+                    {photo.Location} <span>&#183;</span> {photo.Date}
+                </figcaption>
+                </div>
+                ))}
+            </div>
+        
+            <div className="flex flex-col w-1/2 px-1">
+                {column1.map((photo, index) => (
+                <div key={index} className="flex flex-col items-center mb-4">
+                <a href={photo.link} target="_blank" rel="noopener noreferrer">
+                    <img src={photo.link} style={{ width: '100%' }}/>
+                </a>
+                <figcaption className="bg-black text-white italic p-2 text-center w-full">
+                    {photo.Model}, {photo.Lens}
+                    <br />
+                    {photo.Focal} <span>&#183;</span> {photo.Aperture} <span>&#183;</span> {photo.Exposure}s <span>&#183;</span> ISO {photo.ISO}
+                    <br />
+                    {photo.Location} <span>&#183;</span> {photo.Date}
+                </figcaption>
+                </div>
+                ))}
+            </div>
+            </div>
+        </div>
+      );
 };
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const getServerSideProps: GetServerSideProps<
-  ConnectionStatus
-> = async () => {
-  try {
-    await client.connect(); // `await client.connect()` will use the default database passed in the MONGODB_URI
-    return {
-      props: { isConnected: true },
-    };
-  } catch (e) {
-    console.error(e);
-    return {
-      props: { isConnected: false },
-    };
-  }
+export default Images;
+export const getServerSideProps: GetServerSideProps = async () => {
+    try {
+        const client = await clientPromise;
+        const db = client.db("Image-Gallery");
+        const images = await db
+            .collection("image-gallery")
+            .find({})
+            .sort({ Time: -1 })
+            .limit(150)
+            .toArray();
+        return {
+            props: { images: JSON.parse(JSON.stringify(images)) },
+        };
+    } catch (e) {
+        console.error(e);
+        return { props: { images: [] } };
+    }
 };
 
-export default function Home({
-  isConnected,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Pages Router: Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="flex flex-col place-items-center gap-12">
-        <div className="relative flex place-items-center gap-6 before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-          <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          {" + "}
-          <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] saturate-0 brightness-0 dark:saturate-100 dark:brightness-100"
-            src="/mongodb.svg"
-            alt="MongoDB Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-        {isConnected ? (
-          <h2 className="text-lg text-green-500">
-            You are connected to MongoDB!
-          </h2>
-        ) : (
-          <h2 className="text-lg text-red-500">
-            You are NOT connected to MongoDB. Check the <code>README.md</code>{" "}
-            for instructions.
-          </h2>
-        )}
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          This page uses the&nbsp;<strong>Pages Router</strong>. Check out the
-          App Router version here:&nbsp;
-          <Link
-            href="/app-demo"
-            className="underline transition-colors ease-in-out hover:text-green-500"
-          >
-            <code>app/app-demo/page.tsx</code>
-          </Link>
-        </p>
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app&database=mongodb"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js + MongoDB.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fnext.js%2Ftree%2Fcanary%2Fexamples%2Fwith-mongodb&project-name=nextjs-mongodb&repository-name=nextjs-mongodb&integration-ids=oac_jnzmjqM10gllKmSrG0SGrHOH"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+function formatTimecode(timecode: string): string {
+    const date = new Date(timecode);
+    
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: 'short',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric'
+    };
+  
+    return date.toLocaleDateString('en-US', options);
 }
