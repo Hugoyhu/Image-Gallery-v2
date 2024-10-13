82496293275dd85d43af8f3658cd07edb9777352
Hugo Hu <hugo.y.hu@hotmail.com>
2024-10-12 23:03:56 -0400

    add login

---
+++ app/login/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { FormEvent, useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+export default function Login() {
+    const [error, setError] = useState("");
+    const router = useRouter();
+
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const res = await signIn("credentials", {
+            email: formData.get("email"),
+            password: formData.get("password"),
+            redirect: false,
+        });
+        if (res?.error) {
+            setError(res.error as string);
+        }
+        if (res?.ok) {
+            return router.push("/edit");
+        }
+    };
+
+    return (
+        <section className="w-full h-screen flex items-center justify-center">
+            <form
+                className="p-6 w-full max-w-[400px] flex flex-col justify-between items-center gap-2 
+            border border-solid border-black bg-white rounded"
+                onSubmit={handleSubmit}>
+                {error && <div className="text-black">{error}</div>}
+                <h1 className="mb-5 w-full text-2xl font-bold">Sign In</h1>
+                <label className="w-full text-sm">Email</label>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    className="w-full h-8 border border-solid border-black rounded p-2"
+                    name="email" />
+                <label className="w-full text-sm">Password</label>
+                <div className="flex w-full">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="w-full h-8 border border-solid border-black rounded p-2"
+                        name="password" />
+                </div>
+                <button className="w-full border border-solid border-black rounded">
+                    Sign In
+                </button>
+            </form>
+        </section>
+    );
+};
