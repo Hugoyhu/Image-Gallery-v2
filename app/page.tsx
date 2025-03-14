cc365f235ed8540fcc65d319e59e8b7f15c16f59
Hugo Hu <hugo@hugohu.me>
2025-03-14 07:55:48 -0400

    change back to featured to avoid long loading time

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,5 @@ import {useRouter} from "next/navigation";
 
 export default async function Gallery(props: any) {
   const router = useRouter();
-  router.push("/gallery/everything");
+  router.push("/gallery/featured");
 };
